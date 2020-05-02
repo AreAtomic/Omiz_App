eb6410d2a29673e82e07a84d932d67de2b136417
Areatomic <aureliensebe@gmail.com>
2020-05-02 11:24:50 +0200

    Preparation pour build

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const lives = require('./routes/api/liveRoutes');
 const seeding = require('./config/seeding');
 const Live = require('./models/live');
 const axios = require('axios');
+const path = require('path');
 const get_stream = function(timeout = 100000) {
   console.log("start scrapping");
 
@@ -40,6 +41,13 @@ app.use(cors());
 /*
  * Set PORT
  */
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+  })
+}
 const port = process.env.PORT || 5000;
 
 /*
